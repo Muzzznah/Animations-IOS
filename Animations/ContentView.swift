6569b6cf1081bb8045317fa1a721a55378625bab
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-16 00:04:06 -0400

    Completed the steps for Creating explicit animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,31 +13,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
         
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            
-            Spacer()
-
-            Button("Tap Me") {
-                animationAmount += 1
+        Button("Tap Me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
             }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
 
-        //.blur(radius: (animationAmount - 1) * 3)
+        
+//        print(animationAmount)
+//        
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                    .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//            
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(40)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .scaleEffect(animationAmount)
+//        }
+
+//        .blur(radius: (animationAmount - 1) * 3)
 //        .overlay(
 //            Circle()
 //                .stroke(.red)
