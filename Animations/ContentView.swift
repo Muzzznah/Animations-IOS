5d5eb54d535e47a7148cb0f761001dbe2b5903c9
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-15 23:44:24 -0400

    Completed the steps for Animating bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -16,30 +16,42 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            //animationAmount += 1
+        
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
 
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        //.scaleEffect(animationAmount)
+
         //.blur(radius: (animationAmount - 1) * 3)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+//        .overlay(
+//            Circle()
+//                .stroke(.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(2 - animationAmount)
+//                .animation(
+//                    .easeOut(duration: 1)
+//                        .repeatForever(autoreverses: false),
+//                    value: animationAmount
+//                )
+//        )
+//        .onAppear {
+//            animationAmount = 2
+//        }
         
         //.animation(.default, value: animationAmount)
         
